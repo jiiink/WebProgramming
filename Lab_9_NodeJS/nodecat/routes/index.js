f0db593b39acd12378c9c660a0e622314ebfb989
ByeongHyeon Kim <bhkim0404@gmail.com>
2023-12-05 01:03:21 +0900

    .

---
+++ Lab_9_NodeJS/nodecat/routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const { searchByHashtag, getMyPosts, test, getFollow } = require('../controllers');
+
+const router = express.Router();
+
+// POST /test
+router.get('/test', test);
+router.get('/myposts', getMyPosts);
+
+router.get('/search/:hashtag', searchByHashtag);
+
+router.get('/follow', getFollow);
+
+module.exports = router;
